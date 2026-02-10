3d95ee914c251c275cec005889eda38cce8a55d0
vldeycold <tubum02@gmail.com>
2026-02-10 21:22:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://s.shopee.co.id/2B9BanVv16", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029VbCCA6g1NCrOEI15qK07");
+}
+
+function openFacebookPage() {
+    window.open("https://www.facebook.com/profile.php?id=61587095593332");
+}
+const video = document.getElementById("video");
+const overlay = document.getElementById("videoOverlay");
+
+let overlayClicked = false; 
+
+// Overlay muncul di detik tertentu
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 1 && !overlayClicked) {
+        overlay.classList.add("show");
+    }
+});
+
+// Klik overlay
+overlay.addEventListener("click", () => {
+    overlayClicked = true;              
+    overlay.style.display = "none";   
+    overlay.classList.remove("show");
+
+    // Aksi setelah klik
+    window.open("https://s.shopee.co.id/7fTddc6NLI", "_blank");
+});
+
+
+
+
+
+
+
